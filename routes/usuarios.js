31abd792c3a7fa64d46ab1c867ec43d78a6df31b
Rasec Camacho <rasec.camacho19@gmail.com>
2022-11-01 19:27:54 -0700

    validacion personalizada

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,6 +1,7 @@
 
 const { Router } = require('express');
 const { check } = require('express-validator');
+const Role = require('../models/role');
 
 const { validarCampos } = require('../middlewares/validar-campos');
 
@@ -16,7 +17,13 @@ router.post('/', [
     check('nombre', 'El nombre es obligatorio').not().isEmpty(),
     check('contraseña', 'La contraseña debe tener mas de 6 caracteres').isLength({ min: 6 }),
     check('correo', 'El correo no es valido').isEmail(),
-    check('rol', 'No es un rol válido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    // check('rol', 'No es un rol válido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    check('rol').custom( async (rol = '') => {
+        const existeRol = await Role.findOne({ rol });
+        if ( !existeRol ) {
+            throw new Error(`El rol ${ rol } no está registrado en la BD`);
+        }
+    }),
     validarCampos,
 ],usuariosPost);
 
