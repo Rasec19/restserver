5bc941d349d0b6e0a670aadcd4399754b5f7985a
Rasec Camacho <rasec.camacho19@gmail.com>
2022-11-16 01:13:51 -0700

    update

---
+++ controllers/auth.js
@@ -0,0 +1,20 @@
+const { response } = require("express");
+
+
+
+const login = (req, res = response) => {
+
+
+
+    res.json({
+        msg: 'Login ok'
+    });
+
+}
+
+
+
+
+module.exports = {
+    login
+}
