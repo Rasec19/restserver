9fc228e152fde2d4e07be3e4b42e604e6f201417
Rasec Camacho <rasec.camacho19@gmail.com>
2022-11-22 23:31:33 -0700

    update

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -65,9 +65,6 @@ const usuariosDelete = async (req, res = response) => {
 
     const { id } = req.params;
 
-    // Fisicamente lo borramos
-    // const usuario = await Usuario.findByIdAndDelete(id);
-
     const usuario = await Usuario.findByIdAndUpdate( id, { estado: false } );
 
     res.json(usuario);
