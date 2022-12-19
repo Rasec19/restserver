a9efc6252c5729bc89d15042af681ff64edfd4cc
Rasec Camacho <rasec.camacho19@gmail.com>
2022-12-19 11:40:48 -0700

    imagenes

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,24 +1,24 @@
-const path = require('path');
-
 const { response, request } = require('express');
+const { subirArchivo } = require('../helpers');
 
 
-const cargarArchivo = ( req = request, res = response ) => {
+const cargarArchivo = async ( req = request, res = response ) => {
 
     if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
         return res.status(400).json({msg: 'No hay archivos que subir'});
     }
 
-    const { archivo } = req.files;
+    try {
 
-    const uploadPath = path.join( __dirname, '../uploads/', archivo.name);
+        // const nombre = await subirArchivo( req.files, ['txt','md'], 'textos' );
+        const nombre = await subirArchivo( req.files, undefined, 'imgs' );
+        res.status(201).json({
+            nombre,
+        });
 
-    archivo.mv(uploadPath, (err) => {
-        if (err)
-        return res.status(500).json({err});
-
-        res.status(201).json({msg: `File uploaded! ${uploadPath}`});
-    });
+    } catch (msg) {
+        res.status(400).json({msg});
+    }
 };
 
 
